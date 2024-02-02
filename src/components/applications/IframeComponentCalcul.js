1a856a73517fa5d75a20304eba4d7da29de825bc
RadKiSSGaS <108529559+DmitryDemidov48@users.noreply.github.com>
2024-02-02 15:10:10 +0300

    commit.11

--- src/components/applications/IframeComponentCalcul.js
+++ src/components/applications/IframeComponentCalcul.js
@@ -2,11 +2,11 @@ import React from 'react';
 import '../../styles/IframeComponent.css';
 const IframeComponentCalcul = () => {
     return (
-        <div className="video-frame">
+        <div className="calc-frame">
             <div className="slider-container">
                 <div className="slide">
                     <iframe
-                        className="iframe"
+                        className="iframe-calc"
                         src="https://dmitrydemidov48.github.io/calculator_js/"
                         title="Multiroom App"
                     ></iframe>
